518e04972eabd3d18e35563a2930362a2c2d2af7
Winston <winston_nguyenn@hotmail.com>
2023-11-26 20:09:10 -0800

    .

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
 from flask import Flask
-from flask import render_template
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 app = Flask(__name__)
 
+app.config.from_pyfile('settings.py')
 
 @app.route("/")
-def hello_world():
-    return render_template("index.html")
+def index():
+    return "Hello world"
+
+@app.post('/translate')
+def translate():
+    article_en = request.form['original_text']
+    translate_code = request.form['translate_code']
+    access_token = app.config["HUGGINGFACE_TOKEN"]
+    tokenizer = AutoTokenizer.from_pretrained("SnypzZz/Llama2-13b-Language-translate", token=access_token)
+    model = AutoModelForSeq2SeqLM.from_pretrained("SnypzZz/Llama2-13b-Language-translate", token=access_token)
+
+    model_inputs = tokenizer(article_en, return_tensors="pt")
+
+    # translate from English
+    generated_tokens = model.generate(
+        **model_inputs,
+        forced_bos_token_id=tokenizer.lang_code_to_id[translate_code]
+    )
+    translated_sentence = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
+    return translated_sentence[0]
---
+++ settings.py
@@ -0,0 +1,3 @@
+from os import environ
+
+HUGGINGFACE_TOKEN = environ.get("HUGGINGFACE_TOKEN")
